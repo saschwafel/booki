7b2f08876e2768550b302f3cc0d2ff604b97b92e
saschwafel <mr.orange228@gmail.com>
2015-05-17 13:49:55 -0700

    Fixing harcoded URLs, adding Detail view

--- booki/booklist/views.py
+++ booki/booklist/views.py
@@ -1,5 +1,6 @@
 from .models import Book_Entry
-from django.shortcuts import render, HttpResponse
+from django.http import Http404
+from django.shortcuts import render, HttpResponse, get_object_or_404
 from django.template import RequestContext, loader
 
 # Create your views here.
@@ -9,13 +10,31 @@ def index(request):
     # list_of_entries = Book_Entry.objects.order_by('date_added')
     # output = ', '.join([p.title + ' - ' + p.author + '\n' for p in list_of_entries])
     
-    list_of_entries = Book_Entry.objects.order_by('date_added')
-    template = loader.get_template('booklist/index.html')
-    context = RequestContext(request, { 'list_of_entries': list_of_entries, })
+    try:
+
+
+        list_of_entries = Book_Entry.objects.order_by('date_added')
+        template = loader.get_template('booklist/index.html')
+        context = RequestContext(request, { 'list_of_entries': list_of_entries, })
+
+    except Book_Entry.DoesNotExist:
+
+        Http404("Page not found :'(")
+
     return HttpResponse(template.render(context))
 
     # return HttpResponse("You're at the Booklist Index!")
 
 def detail(request, book_entry_id):
-    response = "You are looking at Entry #%s!" 
+    try:
+        entry = get_object_or_404(Book_Entry, pk=book_entry_id)
+        # template = loader.get_template('booklist/index.html')
+        # context = RequestContext(request, { 'list_of_entries': list_of_entries, })
+        return render(request, 'booklist/detail.html', {'book_entry' : entry })
+        # response = "You are looking at Entry #%s!" 
+
+    except Book_Entry.DoesNotExist:
+
+        Http404("Page not found :'(")
+
     return HttpResponse(response % book_entry_id)
