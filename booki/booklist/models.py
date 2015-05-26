dfc0d9cbd80622d1136d64b8a60f3488e9a51bdd
saschwafel <mr.orange228@gmail.com>
2015-05-25 19:10:21 -0700

    Adding cover fetching from goodreads API

--- booki/booklist/models.py
+++ booki/booklist/models.py
@@ -2,13 +2,14 @@ from django.db import models
 
 # Create your models here.
 
+
 class Book_Entry(models.Model):
     title = models.CharField(max_length=300)
     author = models.CharField(max_length=100)
     date_added = models.DateField('Date Added')
     date_started = models.DateField('Date Started', blank=True, null=True)
     date_finished = models.DateField('Date Finished', blank=True, null=True)
-    
+
     def __str__(self):
         return str(self.title)+' - '+ str(self.author)
 
--- booki/booklist/views.py
+++ booki/booklist/views.py
@@ -2,6 +2,9 @@ from .models import Book_Entry
 from django.http import Http404
 from django.shortcuts import render, HttpResponse, get_object_or_404
 from django.template import RequestContext, loader
+import urllib
+import requests
+from bs4 import BeautifulSoup
 
 # Create your views here.
 
@@ -30,7 +33,14 @@ def detail(request, book_entry_id):
         entry = get_object_or_404(Book_Entry, pk=book_entry_id)
         # template = loader.get_template('booklist/index.html')
         # context = RequestContext(request, { 'list_of_entries': list_of_entries, })
-        return render(request, 'booklist/detail.html', {'book_entry' : entry })
+        api_url = "https://www.goodreads.com/search/index.xml?key=" + gr_api_key + "&q=" + urllib.quote_plus(entry.title)
+
+        gr_returned_html = requests.get(api_url)
+        soup = BeautifulSoup(gr_returned_html.text)
+
+        cover = soup.find("image_url").text
+
+        return render(request, 'booklist/detail.html', {'book_entry' : entry, 'cover' : cover, 'api_url' : api_url })
         # response = "You are looking at Entry #%s!" 
 
     except Book_Entry.DoesNotExist:
@@ -38,3 +48,5 @@ def detail(request, book_entry_id):
         Http404("Page not found :'(")
 
     return HttpResponse(response % book_entry_id)
+
+
