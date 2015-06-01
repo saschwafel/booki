102483ab19e36f0afc4d9fd9466e42f1485359d3
saschwafel <saschwafel@gmail.com>
2015-05-31 17:45:01 -0700

    Got datepicker working

--- booki/booklist/forms.py
+++ booki/booklist/forms.py
@@ -1,7 +1,10 @@
 from .models import Book_Entry
 from django import forms
 from django.forms import ModelForm
+from django.contrib.admin.widgets import AdminDateWidget
 from datetimewidget.widgets import DateTimeWidget
+
+
 class EntryForm(ModelForm):
     class Meta:
         model = Book_Entry
@@ -9,6 +12,7 @@ class EntryForm(ModelForm):
         # fields = ['title', 'author']
         # fields =  ('__all__') 
         widgets = {
-            #Use localization and bootstrap 3
-            'datetime': DateTimeWidget(attrs={'id':"date_added"}, usel10n = True, bootstrap_version=3)
+            'date_added': forms.DateInput(attrs={'type':'date'}),
+            'date_started': forms.DateInput(attrs={'type':'date'}),
+            'date_finished': forms.DateInput(attrs={'type':'date'})
         }
